672b17f0495f0fec95aaafb1ff6c73a43d57759d
Chaoshero5567 <79978761+Chaoshero5567@users.noreply.github.com>
2021-11-04 16:33:58 +0100

    Delete src/main directory

--- src/main/java/de/chaos/mc/signsystem/utils/mysql/DefaultSQLValues.java
+++
@@ -1,9 +0,0 @@
-package de.chaos.mc.signsystem.utils.mysql;
-
-public enum DefaultSQLValues {;
-    public static String host = "localhost";
-    public static Long port = 3306L;
-    public static String user ="SignSystem";
-    public static String pw = "SignSystem";
-    public static String database = "SignSystem";
-}
